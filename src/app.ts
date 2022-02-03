6ea3cffb5503cf7453c7fd60dc7781d91cc5585b
avvvet@gmail.com <avvvet@gmail.com>
2022-02-03 16:56:33 +0300

    basic rabbitmq message sending

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('producer server running ..')
+import rabbitMq from 'amqplib'
+import dotenv from 'dotenv'
+dotenv.config()
+
+rabbitMq.connect({
+    protocol: process.env.RABBITMQ_PROTOCOL,
+    hostname: process.env.RABBITMQ_HOST,
+    port: Number(process.env.RABBITMQ_PORT),
+    username: process.env.RABBITMQ_USER,
+    password: process.env.RABBITMQ_PASSWORD,
+    vhost: process.env.RABBITMQ_VHOST
+}).then((connection)=> {
+    connection.createChannel().then((channel) => {
+        const queue = 'AUTH-CHANNEL'
+        const credential = 'yellow'
+        channel.assertQueue(queue)
+        channel.sendToQueue(queue, Buffer.from(credential))
+        console.log('>>>>> sent ')
+    }).catch((err) => {
+        console.log(err)
+    })
+})
